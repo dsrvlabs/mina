09954d0b6003a35a3f5c7b3b1c8b8f61319ab545
sigmoid <alphabet@hotmail.co.kr>
2020-08-27 20:33:27 +0900

    Create 2.run_coda_node.sh

---
+++ 2.run_coda_node.sh
@@ -0,0 +1,1 @@
+coda daemon -discovery-port 8303 -peer /dns4/seed-one.genesis.o1test.net/tcp/10002/ipfs/12D3KooWP7fTKbyiUcYJGajQDpCFo2rDexgTHFJTxCH8jvcL1eAH -peer /dns4/seed-two.genesis.o1test.net/tcp/10002/ipfs/12D3KooWL9ywbiXNfMBqnUKHSB1Q1BaHFNUzppu6JLMVn9TTPFSA -propose-key keys/my-wallet -run-snark-worker $CODA_PK -snark-worker-fee 1
