40acb79b474c0e45297a3e7c55cd61a6b730fe3a
sigmoid <alphabet@hotmail.co.kr>
2020-08-28 19:14:24 +0900

    Update 0.update_scripts.sh

--- 0.update_scripts.sh
+++ 0.update_scripts.sh
@@ -1,3 +1,6 @@
+rm *.sh
+
+wget https://raw.githubusercontent.com/dsrvlabs/coda/master/0.update_scripts.sh
 wget https://raw.githubusercontent.com/dsrvlabs/coda/master/1.generate_key.sh
 wget https://raw.githubusercontent.com/dsrvlabs/coda/master/2.node_install.sh
 wget https://raw.githubusercontent.com/dsrvlabs/coda/master/3.node_run.sh
