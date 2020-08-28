84cbbc744f234a8eb7035a6d7e7585bbd2ef7a9d
sigmoid <alphabet@hotmail.co.kr>
2020-08-28 18:30:56 +0900

    Create 0.update_scripts.sh

---
+++ 0.update_scripts.sh
@@ -0,0 +1,4 @@
+wget https://raw.githubusercontent.com/dsrvlabs/coda/master/1.download_and_install.sh
+wget https://raw.githubusercontent.com/dsrvlabs/coda/master/2.run_coda_node.sh
+
+chmod +x *.sh
