a4464363cc42fa19c07df8e4def9bcd147faa77f
sigmoid <alphabet@hotmail.co.kr>
2020-09-04 12:31:43 +0900

    Update 3.node_run.sh

--- 3.node_run.sh
+++ 3.node_run.sh
@@ -1,1 +1,5 @@
-coda daemon -discovery-port 8303 -peer /dns4/seed-one.genesis.o1test.net/tcp/10002/ipfs/12D3KooWP7fTKbyiUcYJGajQDpCFo2rDexgTHFJTxCH8jvcL1eAH -peer /dns4/seed-two.genesis.o1test.net/tcp/10002/ipfs/12D3KooWL9ywbiXNfMBqnUKHSB1Q1BaHFNUzppu6JLMVn9TTPFSA -propose-key keys/my-wallet -run-snark-worker $CODA_PK -snark-worker-fee 1
+coda daemon -discovery-port 8303 -metrics-port 6060
+#-peer /dns4/seed-one.genesis.o1test.net/tcp/10002/ipfs/12D3KooWP7fTKbyiUcYJGajQDpCFo2rDexgTHFJTxCH8jvcL1eAH 
+#-peer /dns4/seed-two.genesis.o1test.net/tcp/10002/ipfs/12D3KooWL9ywbiXNfMBqnUKHSB1Q1BaHFNUzppu6JLMVn9TTPFSA 
+#-propose-key keys/my-wallet 
+#-run-snark-worker $CODA_PK -snark-worker-fee 1
